93e9bce6412abed5d0c23199629b7ac4f21e812b
Ekaterina Ionidis <79856632+dionispls@users.noreply.github.com>
2025-12-16 19:38:56 +0300

    homework5 fix proba

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Полифилл для clearImmediate
+if (typeof global.clearImmediate === 'undefined') {
+  global.clearImmediate = (handle) => {
+    if (handle && typeof handle === 'object' && handle._onImmediate) {
+      clearTimeout(handle._onImmediate);
+    }
+  };
+}
+
+if (typeof global.setImmediate === 'undefined') {
+  global.setImmediate = (fn, ...args) => {
+    return setTimeout(() => fn(...args), 0);
+  };
+}
